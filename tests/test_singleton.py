436c81d4f7b8192786f616fa1656a3a0c6c82e3c
Enis Bukalo <bukaloenis@gmail.com>
2023-11-06 12:49:46 -0500

    Update test_singleton.py

--- tests/test_singleton.py
+++ tests/test_singleton.py
@@ -13,13 +13,17 @@ def test_singleton():
     obj2 = SingletonTestClass()
     assert obj1 is obj2
 
+    # Both objects should have the same memory location.
     obj1_memory_location = hex(id(obj1))
     obj2_memory_location = hex(id(obj2))
     assert obj1_memory_location == obj2_memory_location
 
+    # Both objects should have the same value for x.
     assert obj1.x == 10
     assert obj2.x == 10
 
+    # A change the value of x in obj1 should reflect in obj2
+    #   since they are both referencing the same memory location.
     obj1.x = 100
     assert obj1.x == 100
     assert obj2.x == 100
