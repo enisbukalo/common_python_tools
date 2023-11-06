abb8b387bc55049f6210be6d9015f85fef0bcd77
Enis Bukalo <bukaloenis@gmail.com>
2023-11-06 12:17:07 -0500

    Added and updated network_tools with ping.

--- network_common/network_tools.py
+++ network_common/network_tools.py
@@ -4,14 +4,15 @@ import os
 
 
 def ping_check(ip_to_check: str, ping_count: int = 1) -> bool:
-    """Responsible determining if IP is reachable.
+    """
+    Ping a given IP address and check if the ping is successful.
 
     Args:
-        ip_to_check (str): IP Address we want to ping.
-        ping_count (int, optional): Times we want to ping IP. Defaults to 1.
+        ip_to_check (str): The IP address to ping.
+        ping_count (int, optional): The number of pings to send. Defaults to 1.
 
     Returns:
-        bool: True if IP is reachable, otherwise False.
+        bool: True if the ping is successful, False otherwise.
     """
     cmd = "-n" if os.name =="nt" else "-c"
     output = subprocess.Popen(f"ping {ip_to_check} {cmd} {ping_count}", stdout=subprocess.PIPE, encoding="utf-8")
---
+++ tests/test_ping.py
@@ -0,0 +1,7 @@
+from network_common import ping_check
+
+def test_ping_check_success():
+    assert ping_check("8.8.8.8") == True
+
+def test_ping_check_fail():
+    assert ping_check("240.0.0.0") == False
