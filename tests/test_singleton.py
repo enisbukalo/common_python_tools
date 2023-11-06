22cd9ee10e8a8640c14be2b1ce5ccc051d16b3fb
Enis Bukalo <bukaloenis@gmail.com>
2023-11-06 12:45:23 -0500

    Update test_singleton.py

--- tests/test_singleton.py
+++ tests/test_singleton.py
@@ -1,14 +1,27 @@
 from class_common import Singleton
 
-class TestClass(Singleton):
+class SingletonTestClass(Singleton):
     def __init__(self) -> None:
         super().__init__()
 
+class BaseTestClass:
+    def __init__(self) -> None:
+        pass
+
 def test_singleton():
-    obj1 = TestClass()
-    obj2 = TestClass()
+    obj1 = SingletonTestClass()
+    obj2 = SingletonTestClass()
     assert obj1 is obj2
 
     obj1_memory_location = hex(id(obj1))
     obj2_memory_location = hex(id(obj2))
     assert obj1_memory_location == obj2_memory_location
+
+def test_base():
+    obj1 = BaseTestClass()
+    obj2 = BaseTestClass()
+    assert obj1 is not obj2
+
+    obj1_memory_location = hex(id(obj1))
+    obj2_memory_location = hex(id(obj2))
+    assert obj1_memory_location != obj2_memory_location
