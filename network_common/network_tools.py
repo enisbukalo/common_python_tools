128fbf1b8890e063fb682010b3e8379f7e5f2334
Enis Bukalo <bukaloenis@gmail.com>
2023-11-06 12:00:54 -0500

    Update

---
+++ network_common/network_tools.py
@@ -0,0 +1,27 @@
+import subprocess
+import re
+import os
+
+IP = "127.0.0.1"
+# IP = "192.168.1.0"
+
+def ping_check(ip_to_check: str, ping_count: int = 1) -> bool:
+    """Responsible determining if IP is reachable.
+
+    Args:
+        ip_to_check (str): IP Address we want to ping.
+        ping_count (int, optional): Times we want to ping IP. Defaults to 1.
+
+    Returns:
+        bool: True if IP is reachable, otherwise False.
+    """
+    cmd = "-n" if os.name =="nt" else "-c"
+    output = subprocess.Popen(f"ping {ip_to_check} {cmd} {ping_count}", stdout=subprocess.PIPE, encoding="utf-8")
+
+    data = ""
+    for line in output.stdout:
+        data = data + line
+
+    return False if len(re.findall("TTL", data)) == 0 else True
+
+print(ping_check(IP, 5))
